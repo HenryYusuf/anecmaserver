9b920aa9ce2e72d35cef2cfe62e5dd406c659338
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-07 22:31:32 +0700

    changes: fix bug

--- app/Http/Controllers/Api/Admin/UploadImageController.php
+++ app/Http/Controllers/Api/Admin/UploadImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Api\BaseController;
 use App\Http\Controllers\Controller;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -11,7 +12,7 @@ class UploadImageController extends BaseController
 {
     public function uploadSingleImage(Request $request)
     {
-        $validator = Validator::make($request, [
+        $validator = Validator::make($request->all(), [
             'file' => 'required',
         ]);
 
@@ -19,7 +20,7 @@ class UploadImageController extends BaseController
             return $this->sendError('Validation Error.', $validator->errors());
         }
 
-        $uploadFileUrl = cloudinary()->upload($request->file('file')->getRealPath(), [
+        $uploadFileUrl = Cloudinary::upload($request->file('file')->getRealPath(), [
             'folder' => 'Edukasi',
             'allowed_formats' => ['jpg', 'jpeg', 'png']
         ])->getSecurePath();

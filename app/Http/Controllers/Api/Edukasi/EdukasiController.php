bc07f674a7fcb558ac504df9ec5224556c3df2f6
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-17 23:25:40 +0700

    changes: edukasi controller

--- app/Http/Controllers/Api/Edukasi/EdukasiController.php
+++ app/Http/Controllers/Api/Edukasi/EdukasiController.php
@@ -20,17 +20,19 @@ class EdukasiController extends BaseController
         $cekResiko = ResikoAnemia::where('user_id', $user->id)->first();
 
         // dd($cekResiko->resiko);
+        if (!$cekResiko) {
+            $results = Edukasi::inRandomOrder()->get();
+            return $this->sendResponse($results, 'Edukasi retrieved successfully.');
+        }
 
         if ($cekResiko->resiko == "rendah") {
             $results = Edukasi::orderByRaw("CASE WHEN kategori = 'pencegahan' THEN 0 ELSE 1 END") // Prioritaskan 'pencegahan'
-                ->orderBy('created_at', 'desc')
+                ->inRandomOrder()
                 ->get();
         } else if ($cekResiko->resiko == "tinggi") {
             $results = Edukasi::orderByRaw("CASE WHEN kategori = 'edukasi' THEN 0 ELSE 1 END") // Prioritaskan 'edukasi'
-                ->orderBy('created_at', 'desc')
+                ->inRandomOrder()
                 ->get();
-        } else {
-            $results = Edukasi::orderBy('created_at', 'desc')->get();
         }
 
         return $this->sendResponse($results, 'Edukasi retrieved successfully.');
